2755ff2e48324d64d1287d0dee92c028cb031dbd
KekToor <p4fczgd@gmail.com>
2021-04-13 19:23:22 +0200

    Verze 4 - Dokončení (prozatimní) hlavní stránky

--- js/selektory.js
+++ js/selektory.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
     $('.fa-chevron-down').hide();
-    $('h2').on('click', function(){
+    $('h2, h4').on('click', function(){
         $(this).nextAll().slideToggle(800);
         $(this).children('.fa').toggleClass('fa-chevron-down fa-chevron-up');
-    })
+    });
 });
