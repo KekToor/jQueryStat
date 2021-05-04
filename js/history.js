471174a0f7f379dc506dc5845718f9eccb9f4f7d
KekToor <p4fczgd@gmail.com>
2021-05-04 18:08:59 +0200

    Verze 6 - Zlepšení vzhledu stránky historie

--- js/history.js
+++ js/history.js
@@ -2,55 +2,55 @@ const udalosti = [
     {
         "rok":"748 př.n.l.",
         "udalost":"Vznik polského opevnění Biskupin.",
-        "detaily":"<strong>Biskupin</strong> je archologické naleziště nacházející se v severním Polsku nedaleko Poznaně. Jedná se o opevnění z pozdní doby bronzové, které nyní slouží také jako venkovní archeologické muzeum. Při objevení se archeologové domnívali, že se jedná slovanské sídlo, později však bylo prokázáno, že se jedná o sídlo příslušníků Biskupínské skupiny lužické kultury. Toto opevnění je jedním z důležitých polských historických památek; od roku 1994 je začleněn do polského kulturního dědictví.",
+        "detaily":"<strong>Biskupin</strong> je archologické naleziště nacházející se v <b>severním Polsku</b> nedaleko <b>Poznaně</b>. Jedná se o opevnění z <i>pozdní doby bronzové</i>, které nyní slouží také jako venkovní archeologické muzeum. Při objevení se archeologové domnívali, že se jedná slovanské sídlo, později však bylo prokázáno, že se jedná o sídlo příslušníků Biskupínské skupiny lužické kultury. Toto opevnění je jedním z důležitých polských historických památek; od roku <b>1994</b> je začleněn do seznamu <i>polského kulturního dědictví</i>.",
         "url":"https://en.wikipedia.org/wiki/Biskupin"
     },
     {
         "rok":"962 - 963",
-        "udalost":"První historicky doložený vládce Polska - Měšek I.",
-        "detaily":"<strong>Měšek I.</strong>, někdy také nazýván <strong>Mečislav</strong>, (asi 935 – 25. května 992) byl nejstarším historicky doloženým polským knížetem z rodu Piastovců. První zpráva o něm pochází z roku 963. Zemřel v roce 992. S jeho vládou bylo spojeno dokončení územního formování polského státu – k pravděpodobně zděděnému Velkopolsku, Mazovsku a oblasti v okolí Sieradze a Łęczyce připojil přinejmenším Slezsko a Západní Pomoří.",
+        "udalost":"Vláda prvního historicky doloženého vládce Polska - Měška I.",
+        "detaily":"<strong>Měšek I.</strong>, někdy také nazýván <strong>Mečislav</strong>, (asi 935 – 25. května 992) byl <b>nejstarším</b> historicky doloženým polským knížetem z rodu <i>Piastovců</i>. První zpráva o něm pochází z roku <b>963</b>. Zemřel v roce <b>992</b>. S jeho vládou bylo spojeno dokončení územního formování polského státu – k pravděpodobně zděděnému <i>Velkopolsku, Mazovsku a oblasti v okolí Sieradze a Łęczyce</i> připojil přinejmenším <i>Slezsko a Západní Pomoří</i>.",
         "url":"https://cs.wikipedia.org/wiki/M%C4%9B%C5%A1ek_I."
     },
     {
         "rok":"966",
         "udalost":"Přijetí křesťanství",
-        "detaily":"Polsko roku <strong>966</strong> přijalo křesťanství, nejednalo se však o pokojný přechod; o desetiletí později roku 1038 následovalo velké pohanské povstání.",
+        "detaily":"Polsko roku <strong>966</strong> přijalo <i>křesťanství</i>, nejednalo se však o pokojný přechod; o desetiletí později roku <b>1038</b> následovalo <b>velké pohanské povstání</b>.",
         "url":"https://cs.wikipedia.org/wiki/Polsko"
     },
     {
         "rok":"1038",
         "udalost":"Velké Pohanské Povstání",
-        "detaily":"Jednalo se o povstání, kvůli němuž musel královský dvůr opustit Hnězdno a uchýlit se do Krakova, který se pak stal hlavním městem na mnoho staletí.",
+        "detaily":"Jednalo se o povstání, kvůli němuž musel královský dvůr opustit <b>Hnězdno</b> a uchýlit se do <b>Krakova</b>, který se pak stal hlavním městem na mnoho staletí.",
         "url":"https://cs.wikipedia.org/wiki/Polsko"
     },
     {
         "rok":"1320",
         "udalost":"Znovusjednocení Polska",
-        "detaily":"Roku 1320 se podařilo sjednotit rozdrobený polský stát <strong>Vladislavu I. Lokýtkovi</strong>. Jeho syn, <strong>Kazimír III. Veliký</strong> (vládl 1333–1370), si vydobyl pověst jednoho z největších polských králů. Rozšířil královskou ochranu Židů a povzbuzoval je k imigraci do Polska. Založil rovněž Krakovskou univerzitu. Posílil moc šlechty (Zlaté svobody) a kodifikoval polské právo. Učinil dohodu s českým králem Karlem IV. – Kazimír se zřekl Slezska na oplátku za to, že čeští králové nebudou usilovat o polskou korunu. Zabránil rozšíření černé smrti do Polska opatřeními na hranicích. S ním ovšem piastovská dynastie vymřela.",
+        "detaily":"Roku 1320 se podařilo sjednotit rozdrobený polský stát <strong>Vladislavu I. Lokýtkovi</strong>. Jeho syn, <strong>Kazimír III. Veliký</strong> (vládl <b>1333–1370</b>), si vydobyl pověst jednoho z největších polských králů. Rozšířil <i>královskou ochranu Židů</i> a povzbuzoval je k imigraci do Polska. Založil rovněž <b>Krakovskou univerzitu</b>. Posílil moc šlechty (Zlaté svobody) a kodifikoval polské právo. Učinil dohodu s českým králem <b>Karlem IV.</b> – Kazimír se zřekl Slezska na oplátku za to, že čeští králové nebudou usilovat o polskou korunu. Zabránil rozšíření černé smrti do Polska opatřeními na hranicích. S ním ovšem piastovská dynastie <b>vymřela</b>.",
         "url":"https://cs.wikipedia.org/wiki/Polsko"
     },
     {
         "rok":"1385",
         "udalost":"Polsko-Litevská Unie (Krewská unie)",
-        "detaily":"Potřeba polsko-litevského spojení vzešla z důvodu ohrožení, které pro obě země představoval na přelomu 14. a 15. století Řád německých rytířů. Unie z Krewu byla první ze šesti unijních smluv uzavřených mezi Polskem a Litvou.",
+        "detaily":"Potřeba polsko-litevského spojení vzešla z důvodu ohrožení, které pro obě země představoval na přelomu 14. a 15. století <b>Řád německých rytířů</b>. Unie z Krewu byla první ze <i>šesti unijních smluv uzavřených mezi Polskem a Litvou</i>.",
         "url":"https://cs.wikipedia.org/wiki/Krevsk%C3%A1_unie"
     },
     {
         "rok":"1648",
         "udalost":"Chmelnického povstání",
-        "detaily":"Chmelnického povstání bylo kozácko-nevolnické povstání na Ukrajině v polovině 17. století. Probíhalo v letech 1648–1654. Bylo namířeno proti polsko-litevskému státu, jehož šlechta se pokoušela omezit rozsah kozácké autonomie.",
+        "detaily":"<b>Chmelnického povstání</b> bylo <i>kozácko-nevolnické povstání</i> na <b>Ukrajině</b> v <b>polovině 17. století</b>. Probíhalo v letech <b>1648–1654</b>. Bylo namířeno proti polsko-litevskému státu, jehož šlechta se pokoušela omezit rozsah kozácké autonomie.",
         "url":"https://cs.wikipedia.org/wiki/Chmelnick%C3%A9ho_povst%C3%A1n%C3%AD"
     },
     {
         "rok":"1806",
         "udalost":"Velkopolské povstání",
-        "detaily":"Velkopolské povstání v roce 1806 vzniklo v té části Polska, kterou při druhém dělení Polska (1793) okupovalo Prusko. Organizoval je generál Jan Henryk Dąbrowski na pomoc francouzské armádě Napoleona Bonaparte, která v rámci tzv. Čtvrté koaliční války bojovala v Prusku. Povstání bylo úspěšné a vedlo ke zřízení Varšavského knížectví pod francouzskou patronací. Po Napoleonově pádu (1814) zřídil Vídeňský kongres z jeho velké části tzv. Kongresové Polsko, zprvu poměrně samostatné.",
+        "detaily":"<b>Velkopolské povstání</b> v roce <b>1806</b> vzniklo v té části Polska, kterou při druhém dělení Polska (1793) okupovalo <i>Prusko</i>. Organizoval je generál <b>Jan Henryk Dąbrowski</b> na pomoc francouzské armádě <b>Napoleona Bonaparte</b>, která v rámci tzv. <i>Čtvrté koaliční války</i> bojovala v Prusku. Povstání bylo úspěšné a vedlo ke zřízení <b>Varšavského knížectví</b> pod francouzskou patronací. Po <i>Napoleonově pádu (1814)</i> zřídil Vídeňský kongres z jeho velké části tzv. <i>Kongresové Polsko</i>, zprvu poměrně samostatné.",
         "url":"https://cs.wikipedia.org/wiki/Velkopolsk%C3%A9_povst%C3%A1n%C3%AD_(1806)"
     },
     {
         "rok":"1939",
         "udalost":"Německá Invaze do Polska",
-        "detaily":"<strong>Invaze do Polska</strong> (též zářijová kampaň nebo zářijové tažení; německy Polenfeldzug [pólenfeldcug], Fall Weiß [fal vajs], polsky Kampania wrześniowa či Wojna obronna 1939 roku) byl útok nacistického Německa a Sovětského svazu na druhou polskou republiku, který zahájil druhou světovou válku v Evropě. Tato invaze začala 1. září 1939, týden po podpisu Paktu Molotov–Ribbentrop mezi Německem a Sovětským svazem a den poté, co pakt schválil Nejvyšší sovět Sovětského svazu. SSSR napadl Polsko 17. září. Kampaň skončila 6. října; Nacistické Německo, Slovenský stát a Sovětský svaz během ní společně v jediném měsíci zlikvidovaly Polsko, které bylo rozděleno a anektováno Německem a Sovětským svazem v souladu s Německo-sovětskou smlouvou o přátelství, spolupráci a vymezení demarkační linie.",
+        "detaily":"<strong>Invaze do Polska</strong> (též zářijová kampaň nebo zářijové tažení; německy <i>Polenfeldzug</i> [pólenfeldcug], <i>Fall Weiß</i> [fal vajs], polsky <i>Kampania wrześniowa či Wojna obronna 1939 roku</i>) byl <i>útok nacistického Německa a Sovětského svazu na druhou polskou republiku</i>, který zahájil <b>druhou světovou válku v Evropě</b>. Tato invaze začala <b>1. září 1939</b>, týden po podpisu Paktu <i>Molotov–Ribbentrop</i> mezi <i>Německem a Sovětským svazem</i> a den poté, co pakt schválil Nejvyšší sovět Sovětského svazu. SSSR napadl Polsko <i>17. září</i>. Kampaň skončila <b>6. října</b>; Nacistické Německo, Slovenský stát a Sovětský svaz během ní společně v jediném měsíci zlikvidovaly Polsko, které bylo rozděleno a anektováno Německem a Sovětským svazem v souladu s <i>Německo-sovětskou smlouvou o přátelství, spolupráci a vymezení demarkační linie</i>.",
         "url":"https://cs.wikipedia.org/wiki/Invaze_do_Polska_(1939)"
     },
 ];
@@ -60,7 +60,7 @@ const hrdinove = [
         "name":"Měšek I.",
         "birth":"~935",
         "death":"25.5.992",
-        "bio":"Měšek I., někdy také nazýván Mečislav, (asi 935 – 25. května 992) byl nejstarším historicky doloženým polským knížetem z rodu Piastovců. První zpráva o něm pochází z roku 963. Zemřel v roce 992. S jeho vládou bylo spojeno dokončení územního formování polského státu – k pravděpodobně zděděnému Velkopolsku, Mazovsku a oblasti v okolí Sieradze a Łęczyce připojil přinejmenším Slezsko a Západní Pomoří.",
+        "bio":"<strong>Měšek I.</strong>, někdy také nazýván <strong>Mečislav</strong>, (asi 935 – 25. května 992) byl <b>nejstarším</b> historicky doloženým polským knížetem z rodu <i>Piastovců</i>. První zpráva o něm pochází z roku <b>963</b>. Zemřel v roce <b>992</b>. S jeho vládou bylo spojeno dokončení územního formování polského státu – k pravděpodobně zděděnému <i>Velkopolsku, Mazovsku a oblasti v okolí Sieradze a Łęczyce</i> připojil přinejmenším <i>Slezsko a Západní Pomoří</i>.",
         "portrait":"mesek.jpg",
         "wiki":"https://cs.wikipedia.org/wiki/M%C4%9B%C5%A1ek_I."
     },
@@ -68,7 +68,7 @@ const hrdinove = [
         "name":"Kazimír III. Veliký",
         "birth":"30.4.1310",
         "death":"5.11.1370",
-        "bio":" Kazimír III. Veliký (vládl 1333–1370), si vydobyl pověst jednoho z největších polských králů. Rozšířil královskou ochranu Židů a povzbuzoval je k imigraci do Polska. Založil rovněž Krakovskou univerzitu. Posílil moc šlechty (Zlaté svobody) a kodifikoval polské právo. Učinil dohodu s českým králem Karlem IV. – Kazimír se zřekl Slezska na oplátku za to, že čeští králové nebudou usilovat o polskou korunu. Zabránil rozšíření černé smrti do Polska opatřeními na hranicích. S ním ovšem piastovská dynastie vymřela.",
+        "bio":"<strong>Kazimír III. Veliký</strong> (vládl <b>1333–1370</b>), si vydobyl pověst jednoho z největších polských králů. Rozšířil <i>královskou ochranu Židů</i> a povzbuzoval je k imigraci do Polska. Založil rovněž <b>Krakovskou univerzitu</b>. Posílil moc šlechty (Zlaté svobody) a kodifikoval polské právo. Učinil dohodu s českým králem <b>Karlem IV.</b> – Kazimír se zřekl Slezska na oplátku za to, že čeští králové nebudou usilovat o polskou korunu. Zabránil rozšíření černé smrti do Polska opatřeními na hranicích. S ním ovšem piastovská dynastie <b>vymřela</b>.",
         "portrait":"kazimir.jpg",
         "wiki":"https://cs.wikipedia.org/wiki/Kazim%C3%ADr_III._Velik%C3%BD"
     },
@@ -76,7 +76,7 @@ const hrdinove = [
         "name":"Mikuláš Koperník",
         "birth":"19.2.1473",
         "death":"24.5.1543",
-        "bio":"Mikuláš Koperník byl astronom, matematik, právník, stratég a lékař, římskokatolický duchovní a tvůrce heliocentrické (sluncestředné) teorie. Jsou po něm pojmenovány krátery na Měsíci (Koperník) a na Marsu (Koperník) a také prvek kopernicium s protonovým číslem 112.",
+        "bio":"<b>Mikuláš Koperník</b> byl <i>astronom, matematik, právník, stratég a lékař, římskokatolický duchovní a tvůrce heliocentrické (sluncestředné) teorie</i>. Jsou po něm pojmenovány krátery na <b>Měsíci</b> (<i>Koperník</i>) a na <b>Marsu</b> (Koperník) a také prvek <b>kopernicium</b> s protonovým číslem <i>112</i>.",
         "portrait":"mikolas.jpg",
         "wiki":"https://cs.wikipedia.org/wiki/Mikul%C3%A1%C5%A1_Kopern%C3%ADk"
     },
@@ -84,7 +84,7 @@ const hrdinove = [
         "name":"Stanisław Żółkiewski",
         "birth":"~1547",
         "death":"7.10.1620",
-        "bio":"Stanisław Żółkiewski byl polský šlechtic erbu Lubicz, magnát a vojenský velitel polsko-litevské Republiky obou národů, který se účastnil mnoha tažení zejména na jižní a východní hranici polsko-litevského státu. Zastával řadu vysokých oficiálních pozic, včetně kastelána Lvova (od roku 1590), vojvody Kyjevského vojvodství a velkého kancléře koruny (od roku 1618). Od roku 1588 zaujímal také úřad korunního polního hejtmana a v roce 1613 byl povýšen na velkého korunního hejtmana. Během své vojenské kariéry zvítězil ve velkých bitvách proti Švédsku, Moskevskému carství, Osmanské říši a Krymskému chanátu. Nejznámější Żółkiewského vítězství bylo proti spojeným ruským a švédským silám v bitvě u Klušina v roce 1610, po které Poláci dobyli a obsadili Moskvu. Zemřel v roce 1620 v bitvě u Cecory proti Turkům poté, co údajně odmítl ustoupit. Již předtím proslul jako vojenský vůdce a jeho hrdinská smrt dál posílila jeho slávu. Je považován za jednoho z nejschopnějších vojenských velitelů v historii Republiky obou národů.",
+        "bio":"<b>Stanisław Żółkiewski</b> byl polský šlechtic erbu <i>Lubicz</i>, magnát a vojenský velitel <b>polsko-litevské Republiky</b> obou národů, který se účastnil mnoha tažení zejména na jižní a východní hranici polsko-litevského státu. Zastával řadu vysokých oficiálních pozic, včetně <b>kastelána Lvova</b> (od roku <i>1590</i>), vojvody <b>Kyjevského vojvodství</b> a <b>velkého kancléře koruny</b> (od roku <i>1618</i>). Od roku <b>1588</b> zaujímal také úřad korunního polního hejtmana a v roce <b>1613</b> byl povýšen na <b>velkého korunního hejtmana</b>. Během své vojenské kariéry zvítězil ve velkých bitvách proti <i>Švédsku, Moskevskému carství, Osmanské říši a Krymskému chanátu</i>. Nejznámější Żółkiewského vítězství bylo proti spojeným ruským a švédským silám v <b>bitvě u Klušina</b> v roce <b>1610</b>, po které Poláci dobyli a obsadili <b>Moskvu</b>. Zemřel v roce 1620 v <b>bitvě u Cecory</b> proti <i>Turkům</i> poté, co údajně odmítl ustoupit. Již předtím proslul jako vojenský vůdce a jeho hrdinská smrt dál posílila jeho slávu. Je považován za jednoho z nejschopnějších vojenských velitelů v historii Republiky obou národů.",
         "portrait":"standa.png",
         "wiki":"https://cs.wikipedia.org/wiki/Stanis%C5%82aw_%C5%BB%C3%B3%C5%82kiewski"
     },
@@ -92,7 +92,7 @@ const hrdinove = [
         "name":"Medvěd Wojtek",
         "birth":"1941",
         "death":"2.12.1963",
-        "bio":"Wojtek byl medvěd syrský adoptovaný vojáky 22. zásobovací roty dělostřelectva 2. polského armádního sboru, jemuž velel generál Anders. Účastnil se Bitvy o Monte Cassino a dosáhl hodnosti desátníka. V dubnu roku 1942 polští vojáci na cestě z Persie do Palestiny za několik konzerv odkoupili malé, hnědé medvídě od perského chlapce. Medvěd ještě neuměl sám jíst a vojáci ho krmili kondenzovaným mlékem zředěným vodou z lahve od vodky, do které byl nakroucený šátek. Možná právě z toho důvodu Wojtek v dospělosti preferoval pít právě ze skleněných lahví. Medvěd byl oficiálně zaevidován do 22. zásobovací roty, se kterou byl v průběhu celé války až po demobilizaci ve Velké Británii.",
+        "bio":"<b>Wojtek</b> byl <b>medvěd syrský</b> adoptovaný vojáky <i>22. zásobovací roty dělostřelectva 2. polského armádního sboru</i>, jemuž velel <b>generál Anders</b>. Účastnil se <b>Bitvy o Monte Cassino</b> a dosáhl hodnosti <b>desátníka</b>. V <i>dubnu roku 1942</i> polští vojáci na cestě z Persie do Palestiny za několik konzerv odkoupili malé, hnědé medvídě od perského chlapce. Medvěd ještě neuměl sám jíst a vojáci ho krmili kondenzovaným mlékem zředěným vodou z lahve od vodky, do které byl nakroucený šátek. Možná právě z toho důvodu Wojtek v dospělosti preferoval pít právě ze skleněných lahví. Medvěd byl oficiálně zaevidován do <i>22. zásobovací roty</i>, se kterou byl v průběhu celé války až po <i>demobilizaci ve Velké Británii</i>.",
         "portrait":"wojtek.jpg",
         "wiki":"https://cs.wikipedia.org/wiki/Stanis%C5%82aw_%C5%BB%C3%B3%C5%82kiewski"
     },
@@ -137,7 +137,7 @@ $(function(){
                             <div>
                             <h3 class = "font-weight-bold">${hrdina.name}</h3>
                             <div class = "hero-popis">
-                                <h5>${hrdina.birth} - ${hrdina.death}</h5>
+                                <h5 class = font-weight-bold>* ${hrdina.birth} &nbsp; &#10015; ${hrdina.death}</h5>
                                 <p class = "px-5">
                                     ${hrdina.bio}
                                 </p>
