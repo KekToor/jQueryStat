dd698d0ed1238dfb6cc2b9867c0bbdf47ffb4274
KekToor <p4fczgd@gmail.com>
2021-05-04 17:21:55 +0200

    Verze 5 - Zprovoznění historie

---
+++ js/history.js
@@ -0,0 +1,161 @@
+const udalosti = [
+    {
+        "rok":"748 př.n.l.",
+        "udalost":"Vznik polského opevnění Biskupin.",
+        "detaily":"<strong>Biskupin</strong> je archologické naleziště nacházející se v severním Polsku nedaleko Poznaně. Jedná se o opevnění z pozdní doby bronzové, které nyní slouží také jako venkovní archeologické muzeum. Při objevení se archeologové domnívali, že se jedná slovanské sídlo, později však bylo prokázáno, že se jedná o sídlo příslušníků Biskupínské skupiny lužické kultury. Toto opevnění je jedním z důležitých polských historických památek; od roku 1994 je začleněn do polského kulturního dědictví.",
+        "url":"https://en.wikipedia.org/wiki/Biskupin"
+    },
+    {
+        "rok":"962 - 963",
+        "udalost":"První historicky doložený vládce Polska - Měšek I.",
+        "detaily":"<strong>Měšek I.</strong>, někdy také nazýván <strong>Mečislav</strong>, (asi 935 – 25. května 992) byl nejstarším historicky doloženým polským knížetem z rodu Piastovců. První zpráva o něm pochází z roku 963. Zemřel v roce 992. S jeho vládou bylo spojeno dokončení územního formování polského státu – k pravděpodobně zděděnému Velkopolsku, Mazovsku a oblasti v okolí Sieradze a Łęczyce připojil přinejmenším Slezsko a Západní Pomoří.",
+        "url":"https://cs.wikipedia.org/wiki/M%C4%9B%C5%A1ek_I."
+    },
+    {
+        "rok":"966",
+        "udalost":"Přijetí křesťanství",
+        "detaily":"Polsko roku <strong>966</strong> přijalo křesťanství, nejednalo se však o pokojný přechod; o desetiletí později roku 1038 následovalo velké pohanské povstání.",
+        "url":"https://cs.wikipedia.org/wiki/Polsko"
+    },
+    {
+        "rok":"1038",
+        "udalost":"Velké Pohanské Povstání",
+        "detaily":"Jednalo se o povstání, kvůli němuž musel královský dvůr opustit Hnězdno a uchýlit se do Krakova, který se pak stal hlavním městem na mnoho staletí.",
+        "url":"https://cs.wikipedia.org/wiki/Polsko"
+    },
+    {
+        "rok":"1320",
+        "udalost":"Znovusjednocení Polska",
+        "detaily":"Roku 1320 se podařilo sjednotit rozdrobený polský stát <strong>Vladislavu I. Lokýtkovi</strong>. Jeho syn, <strong>Kazimír III. Veliký</strong> (vládl 1333–1370), si vydobyl pověst jednoho z největších polských králů. Rozšířil královskou ochranu Židů a povzbuzoval je k imigraci do Polska. Založil rovněž Krakovskou univerzitu. Posílil moc šlechty (Zlaté svobody) a kodifikoval polské právo. Učinil dohodu s českým králem Karlem IV. – Kazimír se zřekl Slezska na oplátku za to, že čeští králové nebudou usilovat o polskou korunu. Zabránil rozšíření černé smrti do Polska opatřeními na hranicích. S ním ovšem piastovská dynastie vymřela.",
+        "url":"https://cs.wikipedia.org/wiki/Polsko"
+    },
+    {
+        "rok":"1385",
+        "udalost":"Polsko-Litevská Unie (Krewská unie)",
+        "detaily":"Potřeba polsko-litevského spojení vzešla z důvodu ohrožení, které pro obě země představoval na přelomu 14. a 15. století Řád německých rytířů. Unie z Krewu byla první ze šesti unijních smluv uzavřených mezi Polskem a Litvou.",
+        "url":"https://cs.wikipedia.org/wiki/Krevsk%C3%A1_unie"
+    },
+    {
+        "rok":"1648",
+        "udalost":"Chmelnického povstání",
+        "detaily":"Chmelnického povstání bylo kozácko-nevolnické povstání na Ukrajině v polovině 17. století. Probíhalo v letech 1648–1654. Bylo namířeno proti polsko-litevskému státu, jehož šlechta se pokoušela omezit rozsah kozácké autonomie.",
+        "url":"https://cs.wikipedia.org/wiki/Chmelnick%C3%A9ho_povst%C3%A1n%C3%AD"
+    },
+    {
+        "rok":"1806",
+        "udalost":"Velkopolské povstání",
+        "detaily":"Velkopolské povstání v roce 1806 vzniklo v té části Polska, kterou při druhém dělení Polska (1793) okupovalo Prusko. Organizoval je generál Jan Henryk Dąbrowski na pomoc francouzské armádě Napoleona Bonaparte, která v rámci tzv. Čtvrté koaliční války bojovala v Prusku. Povstání bylo úspěšné a vedlo ke zřízení Varšavského knížectví pod francouzskou patronací. Po Napoleonově pádu (1814) zřídil Vídeňský kongres z jeho velké části tzv. Kongresové Polsko, zprvu poměrně samostatné.",
+        "url":"https://cs.wikipedia.org/wiki/Velkopolsk%C3%A9_povst%C3%A1n%C3%AD_(1806)"
+    },
+    {
+        "rok":"1939",
+        "udalost":"Německá Invaze do Polska",
+        "detaily":"<strong>Invaze do Polska</strong> (též zářijová kampaň nebo zářijové tažení; německy Polenfeldzug [pólenfeldcug], Fall Weiß [fal vajs], polsky Kampania wrześniowa či Wojna obronna 1939 roku) byl útok nacistického Německa a Sovětského svazu na druhou polskou republiku, který zahájil druhou světovou válku v Evropě. Tato invaze začala 1. září 1939, týden po podpisu Paktu Molotov–Ribbentrop mezi Německem a Sovětským svazem a den poté, co pakt schválil Nejvyšší sovět Sovětského svazu. SSSR napadl Polsko 17. září. Kampaň skončila 6. října; Nacistické Německo, Slovenský stát a Sovětský svaz během ní společně v jediném měsíci zlikvidovaly Polsko, které bylo rozděleno a anektováno Německem a Sovětským svazem v souladu s Německo-sovětskou smlouvou o přátelství, spolupráci a vymezení demarkační linie.",
+        "url":"https://cs.wikipedia.org/wiki/Invaze_do_Polska_(1939)"
+    },
+];
+
+const hrdinove = [
+    {
+        "name":"Měšek I.",
+        "birth":"~935",
+        "death":"25.5.992",
+        "bio":"Měšek I., někdy také nazýván Mečislav, (asi 935 – 25. května 992) byl nejstarším historicky doloženým polským knížetem z rodu Piastovců. První zpráva o něm pochází z roku 963. Zemřel v roce 992. S jeho vládou bylo spojeno dokončení územního formování polského státu – k pravděpodobně zděděnému Velkopolsku, Mazovsku a oblasti v okolí Sieradze a Łęczyce připojil přinejmenším Slezsko a Západní Pomoří.",
+        "portrait":"mesek.jpg",
+        "wiki":"https://cs.wikipedia.org/wiki/M%C4%9B%C5%A1ek_I."
+    },
+    {
+        "name":"Kazimír III. Veliký",
+        "birth":"30.4.1310",
+        "death":"5.11.1370",
+        "bio":" Kazimír III. Veliký (vládl 1333–1370), si vydobyl pověst jednoho z největších polských králů. Rozšířil královskou ochranu Židů a povzbuzoval je k imigraci do Polska. Založil rovněž Krakovskou univerzitu. Posílil moc šlechty (Zlaté svobody) a kodifikoval polské právo. Učinil dohodu s českým králem Karlem IV. – Kazimír se zřekl Slezska na oplátku za to, že čeští králové nebudou usilovat o polskou korunu. Zabránil rozšíření černé smrti do Polska opatřeními na hranicích. S ním ovšem piastovská dynastie vymřela.",
+        "portrait":"kazimir.jpg",
+        "wiki":"https://cs.wikipedia.org/wiki/Kazim%C3%ADr_III._Velik%C3%BD"
+    },
+    {
+        "name":"Mikuláš Koperník",
+        "birth":"19.2.1473",
+        "death":"24.5.1543",
+        "bio":"Mikuláš Koperník byl astronom, matematik, právník, stratég a lékař, římskokatolický duchovní a tvůrce heliocentrické (sluncestředné) teorie. Jsou po něm pojmenovány krátery na Měsíci (Koperník) a na Marsu (Koperník) a také prvek kopernicium s protonovým číslem 112.",
+        "portrait":"mikolas.jpg",
+        "wiki":"https://cs.wikipedia.org/wiki/Mikul%C3%A1%C5%A1_Kopern%C3%ADk"
+    },
+    {
+        "name":"Stanisław Żółkiewski",
+        "birth":"~1547",
+        "death":"7.10.1620",
+        "bio":"Stanisław Żółkiewski byl polský šlechtic erbu Lubicz, magnát a vojenský velitel polsko-litevské Republiky obou národů, který se účastnil mnoha tažení zejména na jižní a východní hranici polsko-litevského státu. Zastával řadu vysokých oficiálních pozic, včetně kastelána Lvova (od roku 1590), vojvody Kyjevského vojvodství a velkého kancléře koruny (od roku 1618). Od roku 1588 zaujímal také úřad korunního polního hejtmana a v roce 1613 byl povýšen na velkého korunního hejtmana. Během své vojenské kariéry zvítězil ve velkých bitvách proti Švédsku, Moskevskému carství, Osmanské říši a Krymskému chanátu. Nejznámější Żółkiewského vítězství bylo proti spojeným ruským a švédským silám v bitvě u Klušina v roce 1610, po které Poláci dobyli a obsadili Moskvu. Zemřel v roce 1620 v bitvě u Cecory proti Turkům poté, co údajně odmítl ustoupit. Již předtím proslul jako vojenský vůdce a jeho hrdinská smrt dál posílila jeho slávu. Je považován za jednoho z nejschopnějších vojenských velitelů v historii Republiky obou národů.",
+        "portrait":"standa.png",
+        "wiki":"https://cs.wikipedia.org/wiki/Stanis%C5%82aw_%C5%BB%C3%B3%C5%82kiewski"
+    },
+    {
+        "name":"Medvěd Wojtek",
+        "birth":"1941",
+        "death":"2.12.1963",
+        "bio":"Wojtek byl medvěd syrský adoptovaný vojáky 22. zásobovací roty dělostřelectva 2. polského armádního sboru, jemuž velel generál Anders. Účastnil se Bitvy o Monte Cassino a dosáhl hodnosti desátníka. V dubnu roku 1942 polští vojáci na cestě z Persie do Palestiny za několik konzerv odkoupili malé, hnědé medvídě od perského chlapce. Medvěd ještě neuměl sám jíst a vojáci ho krmili kondenzovaným mlékem zředěným vodou z lahve od vodky, do které byl nakroucený šátek. Možná právě z toho důvodu Wojtek v dospělosti preferoval pít právě ze skleněných lahví. Medvěd byl oficiálně zaevidován do 22. zásobovací roty, se kterou byl v průběhu celé války až po demobilizaci ve Velké Británii.",
+        "portrait":"wojtek.jpg",
+        "wiki":"https://cs.wikipedia.org/wiki/Stanis%C5%82aw_%C5%BB%C3%B3%C5%82kiewski"
+    },
+];
+
+$(function(){
+    udalosti.forEach((udalost) =>{
+        $('#hist tbody').append(`<tr>
+        <td class="udalost-rok font-weight-bold pt-3">${udalost.rok}</td>
+        <td>
+        <p class="udalost-jmeno">
+            <a href="${udalost.url}" target="_new" class="text-decoration-none udalost-link pl-1">${udalost.udalost}</a>
+            <a class="fa fa-chevron-down float-right text-decoration-none udalost-link" aria-hidden="true" href="#"></a>
+        </p>
+        <p class="udalost-detail">
+          ${udalost.detaily}
+          </p>
+        </td>
+      </tr>`);
+    });
+
+    $('h2 .fa-chevron-down').hide();
+    $('h2').on('click', function(){
+        $(this).nextAll().slideToggle(1500);
+        $(this).children('.fa').toggleClass('fa-chevron-down fa-chevron-up');
+    });
+
+    $('.udalost-detail').hide();
+    $('#hist .fa-chevron-up').hide();
+    $('#hist .udalost-link').on('click',function(){
+        $(this).parent().next().slideToggle(1000);
+        $(this).toggleClass('fa-chevron-down fa-chevron-up');
+     });
+
+
+        hrdinove.forEach((hrdina) =>{
+            $('#hrdinove').append(`
+                <div class="col-lg-6">
+                    <figure class = "text-center">
+                        <img src = "img/${hrdina.portrait}" class = "m-auto udalost-link" style = "height:250px">
+                        <figcaption>
+                            <div>
+                            <h3 class = "font-weight-bold">${hrdina.name}</h3>
+                            <div class = "hero-popis">
+                                <h5>${hrdina.birth} - ${hrdina.death}</h5>
+                                <p class = "px-5">
+                                    ${hrdina.bio}
+                                </p>
+                                <p class = "px-3">
+                                    <b>Zdroj:</b> <a href = "${hrdina.wiki}">Wikipedie</a>
+                                </p>
+                            </div>    
+                            </div>
+                        </figcaption>
+                    </figure>
+                </div>
+            `);
+        });
+        $('.hero-popis').hide();
+        $('#hrdinove img').on('click',function(){
+            $(this).siblings('figcaption').find('.hero-popis').slideToggle(1000);
+         });
+});
+
+
+
